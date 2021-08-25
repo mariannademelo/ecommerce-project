5c8111a09881427cf1f41ab21e715d307d2c0046
Marianna de Melo <alvesmariaanna@gmail.com>
2021-08-25 10:40:39 -0300

    Few minor changes made in SideCart css

--- ecommerce/ecommerce/src/NavBar.js
+++ ecommerce/ecommerce/src/NavBar.js
@@ -162,6 +162,35 @@ function SideCart({setSideCart, cartData, add, setAdd}) {
 
     const [ emptyCart, setEmptyCart ] = useState()
 
+    useEffect(() => {
+        cartData.length === 0 ? setEmptyCart(true) : setEmptyCart(false)
+    }, [cartData])
+
+    return(
+        <>
+        <div className='side-cart_ctnBack back'>
+            <div className='side-cart_ctn'>
+                <h4 
+                onClick={() => setSideCart(false)}
+                className='side-cart_close'>
+                fechar</h4>
+
+                {!emptyCart && 
+                <SideCartContent 
+                add={add}
+                setAdd={setAdd}
+                cartData={cartData}/>}
+
+                {emptyCart && <SideCartContentEmpty/>}
+
+            </div>
+        </div>
+        </>
+    );
+}
+
+function SideCartContent({add, setAdd, cartData}) {
+
     const removeFromCart = (id) => {
         fetch('http://localhost:8000/cart/' + id, {
             method: 'DELETE'
@@ -169,10 +198,6 @@ function SideCart({setSideCart, cartData, add, setAdd}) {
             add === true ? setAdd(false) : setAdd(true)
         })
     }
-
-    useEffect(() => {
-        cartData.length === 0 ? setEmptyCart(true) : setEmptyCart(false)
-    }, [cartData])
 
     const totalPrice = () => {
         let total = 0;
@@ -183,45 +208,46 @@ function SideCart({setSideCart, cartData, add, setAdd}) {
     }
 
     let price = totalPrice()
-
     return(
         <>
-        <div className='side-cart_ctnBack back'>
-            <div className='side-cart_ctn'>
-                <span 
-                onClick={() => setSideCart(false)}>
-                fechar</span>
-                <div className='side-cart_items'>
-                    {cartData.map(product => ( 
-                        <div className='side-cart_item'>
-                            <img src={product.image} alt=''/>
-                            <div className='side-cart_itemDetails'>
-                                <p>{product.item}</p>
-                                <span>Quant.:{product.quantity}</span> 
-                                <span>R${product.price}</span>
-                            </div>
-                            <span 
-                            className='side-cart_remove'
-                            onClick={() => removeFromCart(product.id)}
-                            >X</span>
-                        </div>
-                    ))}
-                </div>
-                <div className='side-cart_total'>
-                    <div className='side-cart_totalValue'>
-                        <span>Total:</span>
-                        <span>R${ price }</span>
+        <div className='side-cart_items'>
+            {cartData.map(product => ( 
+                <div className='side-cart_item'>
+                    <a href={`./${product.itemCode}`}><img src={product.image} alt=''/></a>
+                    <div className='side-cart_itemDetails'>
+                        <a href={`./${product.itemCode}`}>
+                        <p>{product.item}</p></a>
+                        <span>Quant.:{product.quantity}</span> 
+                        <span>R${product.price}</span>
                     </div>
-                    <div className='side-cart_buttons'>
-                        <a href='/carrinho'>
-                        <button className='side-cart_goTo'>Ver Carrinho</button>
-                        </a>
-                        <button className='side-cart_checkout'>Finalizar Compra</button>
-                    </div>
+                    <span 
+                    className='side-cart_remove'
+                    onClick={() => removeFromCart(product.id)}
+                    >X</span>
                 </div>
+            ))}
+        </div>
+        <div className='side-cart_total'>
+            <div className='side-cart_totalValue'>
+                <span>Total:</span>
+                <span>R${ price }</span>
+            </div>
+            <div className='side-cart_buttons'>
+                <a href='/carrinho'>
+                <button className='side-cart_goTo'>Ver Carrinho</button>
+                </a>
+                <button className='side-cart_checkout'>Finalizar Compra</button>
             </div>
         </div>
         </>
+    );
+}
+
+function SideCartContentEmpty() {
+    return (
+        <div className='side-cart_empty'>
+            <h3>Carrinho está vazio!!</h3>
+        </div>
     );
 }
 
